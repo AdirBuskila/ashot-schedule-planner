77ef6b311af4eae30a3df330ad56ccb3107eaa84
Adir Buskila <Adirbu98@gmail.com>
2024-08-10 23:35:43 +0300

    flags

--- src/components/FlagToggleButton.js
+++ src/components/FlagToggleButton.js
@@ -1,30 +1,11 @@
-import React, { useState } from 'react';
-
-const buttonContainerStyles = {
-    display: 'flex',
-    alignItems: 'center',
-    border: '1px solid #ccc',
-    borderRadius: '25px',
-    overflow: 'hidden',
-    cursor: 'pointer',
-};
-
-const labelStyles = (active) => ({
-    padding: '10px',
-    background: active ? '#007BFF' : '#fff',
-    color: active ? '#fff' : '#000',
-    transition: 'background 0.3s, color 0.3s',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100px',
-});
+import React from 'react';
 
 const FlagToggleButton = ({ changeLanguage }) => {
-
     return (
-        <div >
-            <button onClick={changeLanguage}>🇺🇸 - 🇮🇱</button>
+        <div className="flag-toggle-button-container">
+            <button className="flag-toggle-button" onClick={changeLanguage}>
+                🇺🇸 - 🇮🇱
+            </button>
         </div>
     );
 };
